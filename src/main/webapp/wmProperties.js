865a303a9556402aaec8b17e77270fa33ff9837f
root <root@210995f32a0c>
2017-05-22 12:00:38 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "1Native SS Prototype",
+  "homePage" : "Main",
+  "name" : "repo_1Native_SS_Prototype",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
